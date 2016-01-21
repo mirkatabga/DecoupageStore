88e89e4139ad783afb38865169c52974b7f35b8c
Miroslav Hristov <mirkatabga@gmail.com>
2016-01-21 14:49:51 +0200

    Added Html.Submit Helper.

---
+++ Web/DecoupageStore.Web/Extensions/SubmitExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace DecoupageStore.Web.Extensions
+{
+    using System.Collections.Generic;
+    using System.Web.Mvc;
+
+    public static class SubmitExtensions
+    {
+        public static MvcHtmlString Submit(this HtmlHelper helper, string displayText, object htmlAttributes = null)
+        {
+            TagBuilder input = new TagBuilder("input");
+            input.Attributes.Add("type", "submit");
+            input.Attributes.Add("value", displayText);
+
+            var attributes = (IDictionary<string, string>)HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes);
+            input.MergeAttributes(attributes);
+
+            return new MvcHtmlString(input.ToString());
+        }
+    }
+}
